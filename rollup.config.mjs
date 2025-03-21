fbbbdfabfd5453b87410f90f067ffade3d650f43
r.sohrabi <rezasohrabi1995@yahoo.com>
2025-03-22 01:15:52 +0330

    feat: add storybook

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,11 +4,13 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import postcss from "rollup-plugin-postcss";
 
 import pkg from "./package.json" assert { type: "json" };
 
 export default [
   {
+    preserveModules: true,
     input: "src/index.tsx",
     output: [
       { file: pkg.main, format: "cjs", sourcemap: true },
@@ -22,14 +24,28 @@ export default [
       peerDepsExternal(),
       resolve(),
       commonjs(),
-      typescript(),
+      typescript({
+        tsconfig: "./tsconfig.json",
+        exclude: [
+          "**/*.test.tsx",
+          "**/*.test.ts",
+          "**/*.stories.ts",
+          "**/*.stories.tsx",
+        ],
+      }),
+      postcss({
+        extensions: [".css"],
+        extract: false,
+        inject: true,
+        minimize: true,
+      }),
       terser(),
     ],
     external: ["react", "react-dom"],
   },
   {
     input: "src/index.tsx",
-    output: { file: "dist/types/types.d.ts", format: "esm" },
+    output: { file: "dist/types.d.ts", format: "esm" },
     plugins: [dts()],
   },
 ];
