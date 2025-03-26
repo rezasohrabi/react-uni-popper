4018a0477b50684aa151c44402191919aee57253
r.sohrabi <rezasohrabi1995@yahoo.com>
2025-03-26 18:47:23 +0330

    feat: add tooltip component

---
+++ src/Portal.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { createPortal } from "react-dom";
+
+interface PortalProps {
+  children: React.ReactNode;
+  container?: HTMLElement;
+}
+
+export default function Portal({ children, container }: PortalProps) {
+  return createPortal(children, container || document.body);
+}
---
+++ src/types.ts
@@ -0,0 +1,1 @@
+export type PositionType = "top" | "bottom" | "left" | "right";
