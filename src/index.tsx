2b1ecc1c58f196cc98f6cedc06a1a09982d9b2ad
r.sohrabi <rezasohrabi1995@yahoo.com>
2025-03-28 01:17:28 +0330

    feat: add use floating to fix issues of custom implementation

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,26 @@
 import React, {
   useState,
-  useRef,
   useEffect,
   cloneElement,
   ReactElement,
   isValidElement,
   HTMLAttributes,
-} from "react";
-import Portal from "./Portal";
-import { PositionType } from "./types";
-import { useOverlayPosition } from "./useOverlayPosition";
+} from 'react';
+import Portal from './Portal';
+import { PositionType } from './types';
+import {
+  useFloating,
+  flip,
+  offset as offsetMiddleware,
+  shift,
+} from '@floating-ui/react-dom';
 
 export interface TooltipProps
-  extends Omit<HTMLAttributes<HTMLDivElement>, "content"> {
+  extends Omit<HTMLAttributes<HTMLDivElement>, 'content'> {
   children: React.ReactNode;
   className?: string;
   content: React.ReactElement | string;
-  position?: PositionType;
-  enterDelay?: number;
-  leaveDelay?: number;
+  placement?: PositionType;
   offset?: number;
   zIndex?: number;
 }
@@ -26,24 +28,27 @@ export interface TooltipProps
 const Tooltip = ({
   children,
   content,
-  position = "top",
-  className = "",
-  offset = 16,
+  placement = 'top',
+  className = '',
+  offset = 4,
   zIndex,
   ...props
 }: TooltipProps): ReactElement => {
   const [tooltipId] = useState(
     () => `tooltip-${Math.random().toString(36).substr(2, 9)}`,
   );
-  const triggerRef = useRef(null);
-  const tooltipRef = useRef(null);
-  const [show, setShow] = useState(false);
-  const { top, left } = useOverlayPosition({
-    triggerRef,
-    tooltipRef,
-    position,
-    show,
-    offset,
+  const [isOpen, setIsOpen] = useState(false);
+
+  const { floatingStyles, refs } = useFloating({
+    placement,
+    strategy: 'absolute',
+    middleware: [
+      flip(),
+      shift(),
+      offsetMiddleware({
+        mainAxis: offset,
+      }),
+    ],
   });
 
   const childrenElement = isValidElement(children) ? (
@@ -52,30 +57,24 @@ const Tooltip = ({
     <span>{children}</span>
   );
 
-  const showTooltip = () => {
-    setShow(true);
-  };
-
-  const hideTooltip = () => {
-    setShow(false);
-  };
-
+  const showTooltip = () => setIsOpen(true);
+  const hideTooltip = () => setIsOpen(false);
   useEffect(() => {
     const handleKeyDown = (event) => {
-      if (event.key === "Escape") hideTooltip();
+      if (event.key === 'Escape') hideTooltip();
     };
-    document.addEventListener("keydown", handleKeyDown);
+    document.addEventListener('keydown', handleKeyDown);
 
     return () => {
-      document.addEventListener("keydown", handleKeyDown);
+      document.addEventListener('keydown', handleKeyDown);
     };
   }, []);
 
   const childrenProps = {
     ref: (node) => {
-      triggerRef.current = node;
+      refs.setReference(node);
     },
-    "aria-describedby": tooltipId,
+    'aria-describedby': tooltipId,
     onMouseOver: showTooltip,
     onMouseLeave: hideTooltip,
     onFocus: showTooltip,
@@ -87,21 +86,21 @@ const Tooltip = ({
   return (
     <>
       {Trigger}
-      {show && content && (
+      {isOpen && content && (
         <Portal>
           <div
             className="popper"
-            ref={tooltipRef}
+            ref={refs.setFloating}
             id={tooltipId}
             role="tooltip"
             style={{
-              backgroundColor: "red",
+              backgroundColor: 'red',
               zIndex: zIndex || 50,
-              position: "absolute",
+              position: 'absolute',
               top: 0,
               left: 0,
-              inset: "0px auto auto 0px",
-              transform: `translate3d(${left}px, ${top}px, 0px)`,
+              inset: '0px auto auto 0px',
+              ...floatingStyles,
             }}
           >
             <div className={`headless-tooltip ${className}`} {...props}>
