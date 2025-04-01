38fcf9419c2beaae0074c44f1e37dd5ed23e099a
r.sohrabi <rezasohrabi1995@yahoo.com>
2025-04-01 21:02:59 +0330

    fix: eslint react hooks config

--- eslint.config.js
+++ eslint.config.js
@@ -1,41 +1,54 @@
-import { defineConfig } from "eslint/config";
-import globals from "globals";
-import js from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import reactHooks from "eslint-plugin-react-hooks";
-import jsxA11y from "eslint-plugin-jsx-a11y";
-import configPrettier from "eslint-config-prettier";
-import eslintPluginPrettier from "eslint-plugin-prettier";
-import pluginImport from "eslint-plugin-import";
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import configPrettier from 'eslint-config-prettier';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-    languageOptions: { globals: globals.browser },
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+        ...globals.es2021,
+      },
+    },
     plugins: {
       js,
-      pluginReact,
-      "react-hooks": reactHooks,
-      import: pluginImport,
-      "jsx-a11y": jsxA11y,
+      react,
       prettier: eslintPluginPrettier,
     },
     settings: {
       react: {
-        version: "detect",
+        version: 'detect',
       },
     },
-    extends: ["js/recommended", configPrettier],
+    extends: [
+      'js/recommended',
+      react.configs.flat.recommended,
+      jsxA11y.flatConfigs.recommended,
+      reactHooks.configs['recommended-latest'],
+      configPrettier,
+    ],
     ignores: [
-      "**/node_modules/**",
-      "**/dist/**",
-      "**/*.stories.ts",
-      "**/*.stories.tsx",
-      "**/*.test.ts",
-      "**/*.test.tsx",
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/*.stories.ts',
+      '**/*.stories.tsx',
+      '**/*.test.ts',
+      '**/*.test.tsx',
     ],
   },
   tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
 ]);
