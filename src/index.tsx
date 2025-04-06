befea0fb286be92781829fd7f98fcb78172d4be9
r.sohrabi <rezasohrabi1995@yahoo.com>
2025-04-06 12:42:57 +0330

    fix: delay race condition issue

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,115 @@
 import React, {
-  useState,
   cloneElement,
-  ReactElement,
-  isValidElement,
   HTMLAttributes,
+  isValidElement,
+  ReactElement,
   useCallback,
+  useEffect,
   useMemo,
   useRef,
-  useEffect,
+  useState,
 } from 'react';
-import Portal from './components/Portal';
-import { PositionType, TimeoutRef } from './types';
 import {
-  useFloating,
+  autoUpdate,
   flip,
   offset as offsetMiddleware,
   shift,
+  useFloating,
 } from '@floating-ui/react-dom';
-import useOnEscape from './hooks/useOnEscape';
-import { getId, getReactElementRef } from './utils';
+import { createPortal } from 'react-dom';
+
+export type PositionType =
+  | 'top'
+  | 'right'
+  | 'bottom'
+  | 'left'
+  | 'top-start'
+  | 'top-end'
+  | 'right-start'
+  | 'right-end'
+  | 'bottom-start'
+  | 'bottom-end'
+  | 'left-start'
+  | 'left-end';
+
+export type TimeoutRef = ReturnType<typeof setTimeout> | null;
+
+/**
+ * Generates a unique identifier for a tooltip.
+ *
+ * @returns A string in the format `tooltip-<randomString>`, where `<randomString>`
+ * is a randomly generated alphanumeric string of 7 characters.
+ */
+function getId() {
+  return `tooltip-${Math.random().toString(36).slice(2, 9)}`;
+}
+
+function getReactElementRef(
+  element: React.ReactElement,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+): React.Ref<any> | null {
+  // 'ref' is passed as prop in React 19, whereas 'ref' is directly attached to children in older versions
+  if (parseInt(React.version, 10) >= 19) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return (element?.props as any)?.ref || null;
+  }
+  // @ts-expect-error element.ref is not included in the ReactElement type
+  // https://github.com/DefinitelyTyped/DefinitelyTyped/discussions/70189
+  return element?.ref || null;
+}
+
+/**
+ * A custom React hook that listens for the "Escape" key press and triggers a callback function
+ * when the key is pressed, provided the `isOpen` flag is true.
+ *
+ * @param callback - A function to be executed when the "Escape" key is pressed.
+ * @param isOpen - A boolean indicating whether the listener should be active.
+ *
+ * @example
+ * ```tsx
+ * useOnEscape(() => {
+ *   console.log('Escape key pressed!');
+ * }, isOpen);
+ * ```
+ */
+function useOnEscape(callback: () => void, isOpen: boolean) {
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === 'Escape') {
+        callback();
+      }
+    };
+
+    if (isOpen) {
+      window.addEventListener('keydown', handleKeyDown);
+    }
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [callback, isOpen]);
+}
+
+interface PortalProps {
+  children: React.ReactNode;
+  container?: HTMLElement;
+}
+
+/**
+ * A React component that renders its children into a specified DOM container
+ * using React's `createPortal` function. If no container is provided, it defaults
+ * to rendering into the `document.body`.
+ *
+ * @param {PortalProps} props - The props for the Portal component.
+ * @param {React.ReactNode} props.children - The content to be rendered inside the portal.
+ * @param {HTMLElement} [props.container] - The DOM element where the portal content
+ * will be rendered. Defaults to `document.body` if not provided.
+ *
+ * @returns {React.ReactPortal} A React portal that renders the children into the specified container.
+ */
+function Portal({ children, container }: PortalProps) {
+  return createPortal(children, container || document.body);
+}
 
 export interface TooltipProps
   extends Omit<HTMLAttributes<HTMLDivElement>, 'content'> {
@@ -59,7 +150,7 @@ export interface TooltipProps
  *   <button>Hover me</button>
  * </Tooltip>
  */
-const Tooltip = ({
+function Tooltip({
   children,
   content,
   placement = 'top',
@@ -72,7 +163,7 @@ const Tooltip = ({
   disableInteractive = false,
   onOpenChange,
   ...props
-}: TooltipProps): ReactElement => {
+}: TooltipProps): ReactElement {
   const [tooltipId] = useState(getId());
   const [isOpen, setIsOpen] = useState(false);
   const openTimeout = useRef<TimeoutRef>(null);
@@ -98,6 +189,7 @@ const Tooltip = ({
   const { floatingStyles, refs } = useFloating({
     placement,
     strategy: 'absolute',
+    whileElementsMounted: autoUpdate,
     middleware: [
       flip(),
       shift(),
@@ -106,6 +198,39 @@ const Tooltip = ({
       }),
     ],
   });
+
+  function getReactMajorVersion(): number {
+    const version = React.version?.split('.')[0];
+    return parseInt(version || '0', 10);
+  }
+
+  /**
+   * Check if a component is a forward ref component.
+   * @param Component
+   * @returns {boolean}
+   */
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function isForwardRefComponent(Component: any): boolean {
+    const version = getReactMajorVersion();
+
+    if (version >= 19) {
+      // React 19+ forwardRef may not be used at all; ref is passed directly
+      // There's no reliable way to detect if component accepts ref
+      // So just assume it *might* accept ref
+      return true;
+    }
+
+    // React <= 18 use the $$typeof symbol check
+    const forwardRefSymbol =
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      (React as any).ForwardRef || Symbol.for('react.forward_ref');
+
+    return (
+      typeof Component === 'object' &&
+      Component !== null &&
+      Component.type.$$typeof === forwardRefSymbol
+    );
+  }
 
   /**
    * Memoized computation of the children element.
@@ -118,10 +243,13 @@ const Tooltip = ({
    * @param {React.ReactNode} children - The children prop to be rendered.
    * @dependencies [children] - Recomputes only when the `children` prop changes.
    */
-  const childrenElement = useMemo(
-    () => (isValidElement(children) ? children : <span>{children}</span>),
-    [children],
-  );
+
+  const childrenElement = useMemo(() => {
+    if (isValidElement(children) && isForwardRefComponent(children)) {
+      return children;
+    }
+    return <span>{children}</span>;
+  }, [children]);
 
   const isControlled = open !== undefined;
   const isOpenState = isControlled ? open : isOpen;
@@ -132,19 +260,34 @@ const Tooltip = ({
    * - If the tooltip is not controlled (`isControlled` is false), it sets the `isOpen` state to `false`.
    * - Regardless of the control state, it invokes the `onOpenChange` callback with `false` if the callback is provided.
    */
-  const closeTooltip = () => {
-    if (!isControlled) {
-      setIsOpen(false);
-    }
-    onOpenChange?.(false);
-  };
+  const closeTooltip = useCallback(
+    (useDelay: boolean) => {
+      if (!isOpenState) return;
+      if (
+        !isControlled &&
+        ((useDelay && closeTimeout.current !== null) || !useDelay)
+      ) {
+        setIsOpen(false);
+      }
+      onOpenChange?.(false);
+    },
+    [isControlled, isOpenState, onOpenChange],
+  );
 
-  const openTooltip = () => {
-    if (!isControlled) {
-      setIsOpen(true);
-    }
-    onOpenChange?.(true);
-  };
+  const openTooltip = useCallback(
+    (useDelay: boolean) => {
+      if (isOpenState) return;
+
+      if (
+        !isControlled &&
+        ((useDelay && openTimeout.current !== null) || !useDelay)
+      ) {
+        setIsOpen(true);
+      }
+      onOpenChange?.(true);
+    },
+    [isControlled, isOpenState, onOpenChange],
+  );
 
   /**
    * Handles the opening of the tooltip, with an optional delay.
@@ -164,16 +307,17 @@ const Tooltip = ({
     (useDelay = false) => {
       if (closeTimeout.current) {
         clearTimeout(closeTimeout.current);
+        closeTimeout.current = null;
       }
       if (useDelay && openDelay > 0) {
         openTimeout.current = setTimeout(() => {
-          openTooltip();
+          openTooltip(true);
         }, openDelay);
       } else {
-        openTooltip();
+        openTooltip(false);
       }
     },
-    [isControlled, openDelay],
+    [openDelay, openTooltip],
   );
 
   /**
@@ -194,37 +338,41 @@ const Tooltip = ({
     (useDelay = false) => {
       if (openTimeout.current) {
         clearTimeout(openTimeout.current);
+        openTimeout.current = null;
       }
+
       if (useDelay && closeDelay > 0) {
         closeTimeout.current = setTimeout(() => {
-          closeTooltip();
+          closeTooltip(true);
         }, closeDelay);
       } else {
-        closeTooltip();
+        closeTooltip(false);
       }
     },
-    [isControlled, closeDelay],
+    [closeDelay, closeTooltip],
   );
 
   useOnEscape(() => handleClose(false), isOpenState);
 
-  useEffect(() => {
-    return () => {
+  useEffect(
+    () => () => {
       if (openTimeout.current) clearTimeout(openTimeout.current);
       if (closeTimeout.current) clearTimeout(closeTimeout.current);
-    };
-  }, []);
+    },
+    [],
+  );
 
   const childrenProps = useMemo(
     () => ({
-      ref: (node) => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      ref: (node: any) => {
         refs.setReference(node);
         const childRef = getReactElementRef(childrenElement);
 
         if (typeof childRef === 'function') {
           childRef(node);
         } else if (childRef && 'current' in childRef) {
-          childRef.current = node;
+          (childRef as React.MutableRefObject<typeof node>).current = node;
         }
       },
       'aria-describedby': tooltipId,
@@ -257,17 +405,15 @@ const Tooltip = ({
       {isOpenState && content && (
         <Portal>
           <div
-            className="popper"
+            className="headless-popper"
             ref={refs.setFloating}
             id={tooltipId}
             role="tooltip"
             style={{
-              backgroundColor: 'red',
               zIndex: zIndex || 50,
               position: 'absolute',
               top: 0,
               left: 0,
-              inset: '0px auto auto 0px',
               ...floatingStyles,
             }}
             onMouseEnter={
@@ -289,6 +435,6 @@ const Tooltip = ({
       )}
     </>
   );
-};
+}
 
 export default Tooltip;
