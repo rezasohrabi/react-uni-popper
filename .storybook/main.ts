31a9ad75e3ff5f758ba17ac20c5362cafce2f283
r.sohrabi <rezasohrabi1995@yahoo.com>
2025-04-11 16:34:54 +0330

    docs: add a11y addons

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,15 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: [
+    '../stories/**/*.mdx',
+    '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+  ],
   addons: [
     '@storybook/addon-essentials',
-    '@storybook/addon-onboarding',
     '@chromatic-com/storybook',
     '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
   ],
   framework: {
     name: '@storybook/react-vite',
