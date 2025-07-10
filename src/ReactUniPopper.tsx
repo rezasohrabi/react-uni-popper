ddc7dbaf218dcc3cfbb8048ca8ec316a2c3b7f26
r.sohrabi <rezasohrabi1995@yahoo.com>
2025-07-11 00:54:01 +0330

    feat: implement ReactUniPopper component for portal rendering

---
+++ src/ReactUniPopper.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { createPortal } from 'react-dom';
+
+interface PortalProps {
+  children: React.ReactNode;
+  container?: HTMLElement;
+}
+
+/**
+ * A React component that renders its children into a specified DOM container
+ * using React's `createPortal` function. If no container is provided, it defaults
+ * to rendering into the `document.body`.
+ *
+ * @param {PortalProps} props - The props for the Portal component.
+ * @param {React.ReactNode} props.children - The content to be rendered inside the portal.
+ * @param {HTMLElement} [props.container] - The DOM element where the portal content
+ * will be rendered. Defaults to `document.body` if not provided.
+ *
+ * @returns {React.ReactPortal} A React portal that renders the children into the specified container.
+ */
+function Portal({ children, container }: PortalProps) {
+  return createPortal(children, container || document.body);
+}
+
+interface ReactUniPopperProps {
+  portalContainer?: HTMLElement;
+  children: React.ReactNode;
+}
+
+function ReactUniPopper({ portalContainer, children }: ReactUniPopperProps) {
+  return <Portal container={portalContainer}>{children}</Portal>;
+}
+
+export default ReactUniPopper;
--- src/index.tsx
+++ src/index.tsx
@@ -17,7 +17,7 @@ import {
   useFloating,
   arrow as arrowMiddleware,
 } from '@floating-ui/react-dom';
-import { createPortal } from 'react-dom';
+import ReactUniPopper from './ReactUniPopper';
 
 export type PositionType =
   | 'top'
@@ -89,27 +89,6 @@ function useOnEscape(callback: () => void, isOpen: boolean) {
       window.removeEventListener('keydown', handleKeyDown);
     };
   }, [callback, isOpen]);
-}
-
-interface PortalProps {
-  children: React.ReactNode;
-  container?: HTMLElement;
-}
-
-/**
- * A React component that renders its children into a specified DOM container
- * using React's `createPortal` function. If no container is provided, it defaults
- * to rendering into the `document.body`.
- *
- * @param {PortalProps} props - The props for the Portal component.
- * @param {React.ReactNode} props.children - The content to be rendered inside the portal.
- * @param {HTMLElement} [props.container] - The DOM element where the portal content
- * will be rendered. Defaults to `document.body` if not provided.
- *
- * @returns {React.ReactPortal} A React portal that renders the children into the specified container.
- */
-function Portal({ children, container }: PortalProps) {
-  return createPortal(children, container || document.body);
 }
 
 export interface TooltipProps
@@ -480,7 +459,7 @@ function Tooltip({
     <>
       {Trigger}
       {isOpenState && content && (
-        <Portal container={portalContainer}>
+        <ReactUniPopper portalContainer={portalContainer}>
           <div
             className="headless-popper"
             ref={refs.setFloating}
@@ -527,7 +506,7 @@ function Tooltip({
               {content}
             </div>
           </div>
-        </Portal>
+        </ReactUniPopper>
       )}
     </>
   );
